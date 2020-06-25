5349173901ab7b97b6384d99ed4129c3a716870e
gurimmer <s.reianai@gmail.com>
2020-06-25 23:10:45 +0900

    fix header style

--- src/resources/views/app/todo/index.blade.php
+++ src/resources/views/app/todo/index.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 <div class="row">
-    <h2>Todo List</h2>
-    <a href="{{ route('todos.create') }}" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+    <div class="row todo-list">
+        <div class="col s9">
+            <h4 class="main-headline">Todo List</h4>
+        </div>
+        <div class="col s3">
+            <a href="{{ route('todos.create') }}" class="waves-effect waves-light btn-small"><i class="material-icons right">add</i>Add</a>
+        </div>
+    </div>
     @includeWhen($hasTask, 'app.todo.todos', ['tasks' => $tasks])
     @includeWhen(!$hasTask, 'app.todo.empty')
 </div>
